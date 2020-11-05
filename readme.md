a8e74e47691d79afba4d64408a377f21db91c432
grzi <jeremy.thulliez@gmail.com>
2020-11-05 23:44:50 +0100

    feat(graphics): Add some graphics sketches

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Noom under the moon
+
+**Oh, no !**
+
+All the cheese is gone... What will people eat ?! 
+You need to save the world. Quick, take a space ship, and go to the moon, everybody knows that the moon is 
+a big cheese waiting to be eaten.
+
+Another problem, we don't have enough fuel for your trip, you'll need to stop in space satellites to fill up.
+
+Be carefull, we aren't the only ones that want the cheese from the moon, so you may encounter bad guys...
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use amethyst::{
+    core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    ui::{RenderUi, UiBundle},
+    utils::application_root_dir,
+};
+
+pub struct MyState;
+impl SimpleState for MyState {}
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+
+    let resources = app_root.join("assets");
+    let display_config = app_root.join("config/display_config.ron");
+    let key_bindings_path = app_root.join("config/bindings.ron");
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(
+            InputBundle::<StringBindings>::new().with_bindings_from_file(&key_bindings_path)?,
+        )?
+        .with_bundle(UiBundle::<StringBindings>::new())?
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config)?
+                        .with_clear([0.058, 0.219, 0.058, 1.]),
+                )
+                .with_plugin(RenderUi::default())
+                .with_plugin(RenderFlat2D::default()),
+        )?;
+
+    let mut game = Application::new(resources, MyState, game_data)?;
+    game.run();
+
+    Ok(())
 }
