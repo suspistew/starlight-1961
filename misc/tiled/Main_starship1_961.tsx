91cd0854139401d46a287899d2253f198b671010
grzi <jeremy.thulliez@gmail.com>
2020-11-28 11:21:14 +0100

    feat(tiled): Add tiled src to be able to work on lvl on different computers

---
+++ misc/tiled/Main_starship1_961.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Main_starship1_961" tilewidth="16" tileheight="16" tilecount="200" columns="10">
+ <image source="../../assets/sprites/main.png" width="160" height="320"/>
+</tileset>
